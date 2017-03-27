7bbb75c0247007166b284b0aa3437d5d8e0f37bd
Duilio D'Ottavio <duilio.dottavio@gmail.com>
2017-03-27 14:45:23 +0200

    versione iniziale

---
+++ src/duilio/prg/Test.java
@@ -0,0 +1,12 @@
+package duilio.prg;
+
+import javax.swing.JOptionPane;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JOptionPane.showMessageDialog(null, "Ciao");
+	}
+
+}
